d7d463bad813f3448e2bacd3317ad92da97d8ce2
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-08-27 22:37:17 -0600

    Added JS
    -Added SheCodes apiKey
    -Added sheCodes URL to test
    -Added Console log apiKey to test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-alert("hello")
+let apiKey = "389bf9e2068049a646a1betcf8f0o632";
+
+let apiURL =`https://api.shecodes.io/weather/v1/current?query=Salt Lake City&key=${apiKey}&units=metric`
+console.log(apiKey);
