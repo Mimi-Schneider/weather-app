85f75d171f31653b705e199a9727e414a0ee84e9
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-08-29 22:13:00 -0600

    Updating JS and Style
    -Added API data for Salt Lake City instead of Lisbon
    -Added Description
    -Added City
    -Updated style

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 function displayTemperature (response) {
     console.log(response.data.temperature.current);
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description"); 
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+
 }
 
 let apiKey = "389bf9e2068049a646a1betcf8f0o632";
 
-let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=imperial`
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Salt Lake City&key=${apiKey}&units=imperial`
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
