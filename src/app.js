c3838adcc30439cf50a5734ff41f35f646884eb1
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-08-29 22:02:50 -0600

    JS and HTML
    -Updated API to pull current temp (for now SLC)
    -Updated units to Imperial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 function displayTemperature (response) {
     console.log(response.data.temperature.current);
-
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
 let apiKey = "389bf9e2068049a646a1betcf8f0o632";
 
-let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=imperial`
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
