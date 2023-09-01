5238a5f3a471cdb8f41dd4a7fc98bbe5597d6fd2
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-08-31 21:52:11 -0600

    Update app.js
    Updated some of the time formatting

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 function formatDate(timestamp) {
     let date = new Date (timestamp);
     let hours = date.getHours();
+    if (hours <10 ){
+        hours=`0${minutes}`;
+    }
     let minutes = date.getMinutes();
-    let day = date.getDate();
+    if (minutes <10 ){
+        minutes=`0${minutes}`;
+    }
+    let days = 
+    ["Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"];
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
 
