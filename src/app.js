f03a03dd28a877ad626ed7feffeeac2b130a29e3
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-08-31 21:53:03 -0600

    Update app.js
    Removed console log from function that we used for testing

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 
 
 function displayTemperature (response) {
-    console.log(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description"); 
