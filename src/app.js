96140242fda6f2fb00495dfa6f6ac2978c1d828c
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-09-04 12:25:03 -0600

    Update app.js
    Updated JS to shecodes fix

--- src/app.js
+++ src/app.js
@@ -1,61 +1,64 @@
 function formatDate(timestamp) {
-    let date = new Date (timestamp);
+    let date = new Date(timestamp);
     let hours = date.getHours();
-    if (hours <10 ){
-        hours=`0${minutes}`;
+    if (hours < 10) {
+      hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
-    if (minutes <10 ){
-        minutes=`0${minutes}`;
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
     }
-    let days = 
-    ["Sunday", 
-    "Monday", 
-    "Tuesday", 
-    "Wednesday", 
-    "Thursday", 
-    "Friday", 
-    "Saturday"];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
-}
-
-
-function displayTemperature (response) {
+  }
+  
+  function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description"); 
-    let humidityElement=document.querySelector("#humidity");
-    let windElement = document.querySelector ("#wind");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     let feelsElement = document.querySelector("#feels");
-    let dateElement = document.querySelector ("#date");
-    let iconElement = document.querySelector("#current-icon"); 
-
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#current-icon");
+  
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = response.data.wind.speed;
     feelsElement.innerHTML = Math.round(response.data.temperature.feels_like);
-    dateElement.innerHTML = formatDate(response.data.time*1000);
-    iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
-    iconElement.setAttribute("alt",response.data.condition.icon);
-}
-
-
-function search(city) {
-    let apiKey = "389bf9e2068049a646a1betcf8f0o632";
-    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute(
+      "src",
+      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    );
+    iconElement.setAttribute("alt", response.data.condition.icon);
+  }
+  
+  function search(city) {
+    let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    console.log(apiUrl);
     axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSubmit(event) {
+  }
+  
+  function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-}
-
-search("Salt Lake City");
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit)", handleSubmit);
+  }
+  
+  search("Salt Lake City");
+  
+  let form = document.querySelector("form");
+  form.addEventListener("submit", handleSubmit);
