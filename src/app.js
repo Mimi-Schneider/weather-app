81dc93c1273fd76148f1d0a2b154e15585a3b0c5
Mimi-Schneider <115606654+Mimi-Schneider@users.noreply.github.com>
2023-10-05 16:09:54 -0600

    Update app.js
    Added Days in array for forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){ 
+  let date = new Date (timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  return days[day]
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -28,8 +36,9 @@ function displayForecast(response) {
   forecast.forEach(function(forecastDay){
     forecastHTML = forecastHTML + `
   <div class="col-2">
-  <div class = "WeatherForecastPreview"> 
-    <div class="forecast-time">${forecastDay.time}</div>
+  <div class = "WeatherForecastPreview"> ${formatDay
+    (forecastDay.time)} </div>
+    <div class="forecast-time">${forecastDay.time}
 <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" 
 alt="" 
 width="36"/>
